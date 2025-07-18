7435ceed66d13896b946e9eb3565a29479f9dc9e
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2025-07-18 19:04:06 +0000

    update: ft_strlen関数の実装を簡素化し、ループ処理を削除

--- src/ft_strlen.s
+++ src/ft_strlen.s
@@ -2,14 +2,11 @@ section .text
   global ft_strlen
 
 ft_strlen:
-  ; rdi points to the string
-  mov rax, 0
-
-.loop:
-  cmp byte [rdi + rax], 0
-  je .done
-  inc rax
-  jmp .loop
-
-.done:
+  mov rcx, -1
+  mov al, 0
+  cld
+  repne scasb
+  not rcx
+  dec rcx
+  mov rax, rcx
   ret
