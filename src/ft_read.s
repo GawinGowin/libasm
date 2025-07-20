52f8be716dff2c452e8d98590e93a8bc660a4bb7
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2025-07-20 16:43:00 +0000

    update: ft_read関数のコードを整形し、可読性を向上

--- src/ft_read.s
+++ src/ft_read.s
@@ -1,29 +1,24 @@
 section .text
-    global ft_read
-    extern __errno_location
+  global ft_read
+  extern __errno_location
 
 ft_read:
-    push rbp
-    mov rbp, rsp
-    
-    mov rax, 0          ; syscall number for read
-    ; rdi = fd (already set)
-    ; rsi = buf (already set)
-    ; rdx = count (already set)
-    syscall
-    
-    cmp rax, 0          ; check if return value is negative
-    jl .error           ; if negative, handle error
-    
-    leave
-    ret
-    
+  enter 0, 0
+  mov rax, 0
+  syscall
+
+  cmp rax, 0
+  jl .error
+
+  leave
+  ret
+
 .error:
-    neg rax             ; make error code positive
-    push rax            ; save error code
-    call __errno_location wrt ..plt
-    pop rdx             ; restore error code
-    mov [rax], rdx      ; set errno
-    mov rax, -1         ; return -1
-    leave
-    ret
+  neg rax
+  push rax
+  call __errno_location wrt ..plt
+  pop rdx
+  mov [rax], rdx
+  mov rax, -1
+  leave
+  ret
