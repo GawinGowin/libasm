f785c56e77a8a28f00f2deb3d5f50b2d562d6c5e
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2025-07-20 16:40:05 +0000

    update: ft_write関数のコードを簡素化し、不要なスタックフレーム管理を削除

--- src/ft_write.s
+++ src/ft_write.s
@@ -1,29 +1,24 @@
 section .text
-    global ft_write
-    extern __errno_location
+  global ft_write
+  extern __errno_location
 
 ft_write:
-    push rbp
-    mov rbp, rsp
+  enter 0, 0
+  mov rax, 1
+  syscall
     
-    mov rax, 1          ; syscall number for write
-    ; rdi = fd (already set)
-    ; rsi = buf (already set)
-    ; rdx = count (already set)
-    syscall
+  cmp rax, 0
+  jl .error
     
-    cmp rax, 0          ; check if return value is negative
-    jl .error           ; if negative, handle error
-    
-    leave
-    ret
+  leave
+  ret
     
 .error:
-    neg rax             ; make error code positive
-    push rax            ; save error code
-    call __errno_location wrt ..plt
-    pop rdx             ; restore error code
-    mov [rax], rdx      ; set errno
-    mov rax, -1         ; return -1
-    leave
-    ret
+  neg rax
+  push rax
+  call __errno_location wrt ..plt
+  pop rdx
+  mov [rax], rdx
+  mov rax, -1
+  leave
+  ret
