7542e6da9e30512c24c57c497c7e41a661f31f92
GawinGowin <101625248+GawinGowin@users.noreply.github.com>
2025-07-24 00:12:55 +0000

    update: ft_atoi_baseの実装を改善し、コードの可読性を向上

--- src/ft_atoi_base.s
+++ src/ft_atoi_base.s
@@ -1,36 +1,34 @@
 section .text
     global ft_atoi_base
+    extern ft_isspace
+    extern ft_strchr
+    extern ft_strspn
 
-; int ft_atoi_base(char *str, char *base)
-; rdi = str, rsi = base
 ft_atoi_base:
     push rbp
     mov rbp, rsp
-    sub rsp, 32                 ; Local variables space
+    sub rsp, 32
     push rbx
     push r12
     push r13
     push r14
     push r15
     
-    mov r12, rdi                ; r12 = str
-    mov r13, rsi                ; r13 = base
+    mov r12, rdi
+    mov r13, rsi
     
-    ; Check if base is valid
     mov rdi, r13
     call base_check_failed
     test eax, eax
     jnz .return_zero
     
-    ; Initialize variables
-    mov dword [rbp-4], 1        ; sign = 1
-    lea rsi, [rbp-4]            ; &sign
-    mov rdi, r12                ; str
+    mov dword [rbp-4], 1
+    lea rsi, [rbp-4]
+    mov rdi, r12
     call strip_spaces_sign
-    mov r14d, eax               ; r14 = index
+    mov r14d, eax
     
-    ; Calculate base_num (strlen of base)
-    mov r15, 0                  ; base_num = 0
+    mov r15, 0
 .count_base:
     movzx eax, byte [r13 + r15]
     test al, al
@@ -38,31 +36,27 @@ ft_atoi_base:
     inc r15
     jmp .count_base
 .count_done:
-    ; r15 = base_num
     
-    mov rbx, 0                  ; sum = 0
+    mov rbx, 0
     
 .convert_loop:
-    ; Check if current char is in base
-    movzx edi, byte [r12 + r14] ; str[index]
-    mov rsi, r13                ; base
+    movzx edi, byte [r12 + r14]
+    mov rsi, r13
     call is_included
     cmp eax, -1
     je .conversion_done
     
-    ; sum = sum * base_num + digit_value
-    mov rcx, rax                ; rcx = digit_value (from is_included return)
+    mov rcx, rax
     mov rax, rbx
-    mul r15                     ; rax = sum * base_num
+    mul r15
     mov rbx, rax
-    add rbx, rcx                ; rbx += digit_value
-    inc r14d                    ; index++
+    add rbx, rcx
+    inc r14d
     jmp .convert_loop
     
 .conversion_done:
-    ; Apply sign
-    mov eax, dword [rbp-4]      ; sign
-    imul rbx, rax               ; sum *= sign
+    mov eax, dword [rbp-4]
+    imul rbx, rax
     mov rax, rbx
     jmp .exit
     
@@ -78,38 +72,36 @@ ft_atoi_base:
     leave
     ret
 
-; int strip_spaces_sign(char *str, int *sign)
-; rdi = str, rsi = sign
 strip_spaces_sign:
     push rbp
     mov rbp, rsp
     push rbx
+    push r12
     
-    mov rbx, rsi                ; rbx = sign pointer
-    xor eax, eax                ; i = 0
+    mov rbx, rsi
+    mov r12, rdi
+    xor eax, eax
     
 .skip_spaces:
-    movzx ecx, byte [rdi + rax]
-    ; Check if char is whitespace ('\t' to '\r' or ' ')
-    cmp cl, 0x09                ; '\t'
-    jl .check_space
-    cmp cl, 0x0D                ; '\r'
-    jle .is_space
-.check_space:
-    cmp cl, 0x20                ; ' '
-    jne .spaces_done
-.is_space:
-    inc eax
+    movzx edi, byte [r12 + rax]
+    push rax
+    call ft_isspace
+    pop rcx
+    test eax, eax
+    jz .spaces_done
+    inc rcx
+    mov eax, ecx
     jmp .skip_spaces
     
 .spaces_done:
-    mov dword [rbx], 1          ; *sign = 1
+    mov dword [rbx], 1
+    mov eax, ecx
     
 .sign_loop:
-    movzx ecx, byte [rdi + rax]
-    cmp cl, '+'
+    movzx edx, byte [r12 + rax]
+    cmp dl, '+'
     je .plus_sign
-    cmp cl, '-'
+    cmp dl, '-'
     je .minus_sign
     jmp .sign_done
     
@@ -118,56 +110,52 @@ strip_spaces_sign:
     jmp .sign_loop
     
 .minus_sign:
-    neg dword [rbx]             ; *sign *= -1
+    neg dword [rbx]
     inc eax
     jmp .sign_loop
     
 .sign_done:
+    pop r12
     pop rbx
     leave
     ret
 
-; int is_included(char s, char *base)
-; rdi = s (char), rsi = base
 is_included:
     push rbp
     mov rbp, rsp
+    push rbx
+    push r12
     
-    xor eax, eax                ; i = 0
+    mov bl, dil
+    mov r12, rsi
+    mov rdi, rsi
+    movzx esi, bl
+    call ft_strchr
     
-.search_loop:
-    movzx ecx, byte [rsi + rax]
-    test cl, cl
+    test rax, rax
     jz .not_found
     
-    cmp dil, cl                 ; Compare s with base[i]
-    je .found
-    
-    inc eax
-    jmp .search_loop
-    
-.found:
-    ; Return index (already in eax)
-    leave
-    ret
+    sub rax, r12
+    jmp .done
     
 .not_found:
     mov eax, -1
+    
+.done:
+    pop r12
+    pop rbx
     leave
     ret
 
-; int base_check_failed(char *base)
-; rdi = base
 base_check_failed:
     push rbp
     mov rbp, rsp
     push rbx
     push r12
     
-    mov rbx, rdi                ; rbx = base
+    mov rbx, rdi
     
-    ; Count base length
-    xor eax, eax                ; i = 0
+    xor eax, eax
 .count_loop:
     movzx ecx, byte [rbx + rax]
     test cl, cl
@@ -175,40 +163,35 @@ base_check_failed:
     inc eax
     jmp .count_loop
 .count_done:
-    ; Check if length <= 1
     cmp eax, 1
     jle .return_fail
     
-    mov r12d, eax               ; r12 = base_len
-    xor eax, eax                ; i = 0
+    mov r12d, eax
+    xor eax, eax
     
 .outer_loop:
     cmp eax, r12d
     jge .check_passed
     
-    ; Check if base[i] is valid character
     movzx edi, byte [rbx + rax]
     push rax
     call valid_char_failed
-    pop rcx                     ; Restore i to rcx
+    pop rcx
     test eax, eax
     jnz .return_fail
     
-    ; Check for duplicates
-    mov edx, ecx                ; j = i + 1
+    mov edx, ecx
     inc edx
 .inner_loop:
     cmp edx, r12d
     jge .inner_done
     
-    movzx esi, byte [rbx + rcx] ; base[i]
-    movzx edi, byte [rbx + rdx] ; base[j]
+    movzx esi, byte [rbx + rcx]
+    movzx edi, byte [rbx + rdx]
     
-    ; Check if base[i] == base[j]
     cmp esi, edi
     je .return_fail
     
-    ; Check if base[j] is valid
     push rcx
     push rdx
     call valid_char_failed
@@ -226,11 +209,11 @@ base_check_failed:
     jmp .outer_loop
     
 .check_passed:
-    xor eax, eax                ; Return 0 (success)
+    xor eax, eax
     jmp .exit
     
 .return_fail:
-    mov eax, 1                  ; Return 1 (fail)
+    mov eax, 1
     
 .exit:
     pop r12
@@ -238,32 +221,33 @@ base_check_failed:
     leave
     ret
 
-; int valid_char_failed(char s)
-; rdi = s (char)
 valid_char_failed:
     push rbp
     mov rbp, rsp
+    push rbx
     
-    ; Check if s == '+' or s == '-'
-    cmp dil, '+'
+    mov bl, dil
+    
+    cmp bl, '+'
     je .return_fail
-    cmp dil, '-'
-    je .return_fail
-    
-    ; Check if s <= ' ' (0x20)
-    cmp dil, 0x20
-    jle .return_fail
-    
-    ; Check if s == 127 (DEL)
-    cmp dil, 127
+    cmp bl, '-'
     je .return_fail
     
-    ; Character is valid
+    movzx edi, bl
+    call ft_isspace
+    test eax, eax
+    jnz .return_fail
+    
+    cmp bl, 127
+    je .return_fail
+    
     xor eax, eax
-    leave
-    ret
+    jmp .done
     
 .return_fail:
     mov eax, 1
+    
+.done:
+    pop rbx
     leave
     ret
